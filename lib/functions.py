99dd071c5d0e549bce82caf2e4f873bd7d4eeed6
Emmakaranja1 <karanjaemmak@gmail.com>
2025-05-07 22:25:07 +0300

    Add greet_programmer function that prints a default greeting Add greet function that prints a personalized greeting Add greet_with_default function with optional name parameter Add add function to return the sum of two numbers Add halve function to return half the value of a number

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
+
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
+
 def halve(number):
+    return number / 2
     pass
